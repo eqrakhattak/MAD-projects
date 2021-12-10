1d931ddb029ae3ed938a878f5c0a4f92db695506
eqrakhattak <eqra.khattak7@gmail.com>
2021-12-10 15:50:02 +0500

    Theory Assignment # 3

--- Intents/app/src/main/java/com/eqra/intents/MainActivity.kt
+++ Intents/app/src/main/java/com/eqra/intents/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.eqra.intents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
