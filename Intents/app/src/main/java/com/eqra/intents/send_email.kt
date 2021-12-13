0023e44c11f5d33fa69c5ee4ffdd7909a6da69e0
Laiba-Arooj <70106509+Laiba-Arooj@users.noreply.github.com>
2021-12-13 20:49:08 +0500

    Two intents by Laiba : Added

---
+++ Intents/app/src/main/java/com/eqra/intents/send_email.kt
@@ -0,0 +1,40 @@
+package com.eqra.intents
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.eqra.intents.databinding.ActivitySendEmailBinding
+
+class send_email : AppCompatActivity() {
+    private lateinit var binding : ActivitySendEmailBinding
+
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySendEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendMail.setOnClickListener{
+            val email = binding.emailAddress.text.toString()
+            val subject = binding.etSubject.text.toString()
+            val bodyMsg = binding.etMessage.text.toString()
+            val addresses = email.split(",".toRegex()).toTypedArray()
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // only email apps should handle this
+                putExtra(Intent.EXTRA_EMAIL, addresses)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, bodyMsg)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this@send_email,"Required App is not installed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
